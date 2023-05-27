4f7a41bfa15e17692476605859ce7422abce55e1
Mariam Adel <mariamadel9420@gmail.com>
2023-05-27 03:22:47 +0300

    correct annotation step by add die to convert2annovar.pl

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-# Step 1: Download fastq files from SRA
-# Replace 'file.fastq' with the actual file name
-fastq_file="file.fastq"
-# Add commands to download the fastq file from SRA
-# For example: wget https://www.ncbi.nlm.nih.gov/sra/fastq-file.fastq.gz
+# Step 1: Replace 'file.fastq' with the actual file name provided through terminal input
+fastq_file="$1"
 
 # Step 2: Check quality with fastqc
 fastqc "$fastq_file"
 
-# Step 3: Download and install Trimmomatic
-trimmomatic_url="http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip"
-trimmomatic_dir="Trimmomatic-0.39"
+# Step 3: Use existing Trimmomatic installation
 trimmomatic_jar="tools/trimmomatic.jar"
-
-# Download Trimmomatic
-wget "$trimmomatic_url"
-
-# Unzip the downloaded file
-unzip "$trimmomatic_dir.zip"
-
-# Move Trimmomatic jar file to the tools folder
-mv "$trimmomatic_dir/trimmomatic-0.39.jar" "$trimmomatic_jar"
-
-# Make Trimmomatic jar file executable
-chmod +x "$trimmomatic_jar"
-
-# Test Trimmomatic installation
-java -jar "$trimmomatic_jar"
 
 # Step 4: Index the human genome
 reference_genome="reference2/Homo_sapiens_assembly38.fasta"
@@ -51,11 +31,15 @@ bcftools mpileup -f "$reference_genome" "$sorted_bam" | bcftools call -mv -Ov > 
 
 # Step 9: Annotation
 output_avinput="output.avinput"
+output_avinput_function="output.avinput.variant_function"
 annovar_dir="tools/annovar"
-convert2annovar.pl -format vcf4 "$variants_vcf" > "$output_avinput"
-"$annovar_dir/annotate_variation.pl" -geneanno -dbtype refGene "$output_avinput" "$annovar_dir/humandb/"
+"$annovar_dir/convert2annovar.pl" -format vcf4 "$variants_vcf" > "$output_avinput"
+"$annovar_dir/annotate_variation.pl" -geneanno -dbtype refGene "$output_avinput" "$annovar_dir/humandb/" >  "$output_avinput_function"
 
 # Step 10: Grep
 lynch_positions_file="Lynch_positions_in_file.txt"
-output_file="output_file.hg38_multianno.txt"
-grep -E 'MLH1|MSH2|MSH6|PMS2|EPCAM' "$output_file" > "$lynch_positions_file"
+grep -E 'MLH1|MSH2|MSH6|PMS2|EPCAM' "$output_avinput_function" > "$lynch_positions_file"
+
+
+# Clean up by deleting unnecessary files
+rm -f "$aligned_sam" "$aligned_bam" "$variants_vcf" "$output_avinput" "$output_file" "$output_avinput_function"
